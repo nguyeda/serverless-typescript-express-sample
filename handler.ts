63669ed52813b4a4aeba7edf50aee3fa9942abcd
David Nguyen <david@netis.be>
2018-06-19 12:09:26 +0200

    upgrade packages, upgrade to webpack 4 config

--- handler.ts
+++ handler.ts
@@ -6,7 +6,7 @@ export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Cal
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
       input: event,
-    }),
+    }, null, 2),
   };
 
   cb(null, response);
